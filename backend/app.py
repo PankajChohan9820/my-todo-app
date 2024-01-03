f1fee4f74a6b46580db9458d794e6affddc87694
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-02 19:54:13 -0800

    Backend setup for my todo task I have added initial setup for Todo and Users routes, configured app.py, and set up models using SQLAlchemy Things added are - Implement Todo and Users routes to handle basic CRUD operations. - Configure app.py to initialize the Flask application and set up necessary dependencies. - Establish the initial database models using SQLAlchemy for tasks todo items and user information. - Set up basic configuration settings for the application.
    This commit lays the groundwork for the application structure, routing, and database models.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_cors import CORS
+from models.task_model import db
+from routes.todo_route import tasks_api
+from routes.todo_users import users_api
+
 
 app = Flask(__name__)
+CORS(app = app)
+app.config.from_object('config.Config')
+db.init_app(app)
 
+
+app.register_blueprint(tasks_api)
+app.register_blueprint(users_api)
+
+
+# Hello world to test
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug = True)
---
+++ backend/routes/todo_route.py
@@ -0,0 +1,71 @@
+from flask import Blueprint, jsonify, request
+from models.task_model import db, Task
+tasks_api = Blueprint('tasks_api', __name__)
+
+
+@tasks_api.route('/tasks', methods=['GET'])
+def get_tasks():
+    try:
+        tasks = Task.query.all()
+        task_list = [task.to_dict() for task in tasks]
+        return jsonify(task_list), 200
+    except Exception as e:
+        return jsonify({'error':str(e)}), 404
+
+@tasks_api.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    try:
+        task = Task.query.get(task_id)
+        if task:
+            return jsonify(task.to_dict()), 200
+        else:
+            return jsonify({'error': 'Task not found'}), 404
+    except Exception as e:
+        return jsonify({'error':str(e)}), 404
+
+@tasks_api.route('/tasks', methods=['POST'])
+def add_task():
+    try:
+        data = request.get_json()
+        title = data.get('title')
+        description = data.get('description')
+
+        task = Task(title=title, description=description)
+        db.session.add(task)
+        db.session.commit()
+
+        return jsonify({'message': 'Task added successfully'}), 200
+    except Exception as e:
+        return jsonify({'error':str(e)}), 404
+
+@tasks_api.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    try:
+        task = Task.query.get(task_id)
+        if task:
+            data = request.get_json()
+            title = data.get('title')
+            description = data.get('description')
+
+            task.title = title
+            task.description = description
+            db.session.commit()
+
+            return jsonify({'message': 'Task updated successfully'}), 200
+        else:
+            return jsonify({'error': 'Task not found'}), 404
+    except Exception as e:
+        return jsonify({'error':str(e)}), 404
+
+@tasks_api.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    try:
+        task = Task.query.get(task_id)
+        if task:
+            db.session.delete(task)
+            db.session.commit()
+            return jsonify({'message': 'Task deleted successfully'}), 200
+        else:
+            return jsonify({'error': 'Task not found'}), 404
+    except Exception as e:
+        return jsonify({'error':str(e)}), 404
