d99dccfd43e68b3b0d77535e87956773ec1397a8
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-03 18:40:05 -0800

    Backend update in database - added timestamp for creation of todo list

--- backend/models/task_model.py
+++ backend/models/task_model.py
@@ -6,13 +6,18 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.String(255))
+    completed = db.Column(db.Boolean)
+    timestamp = db.Column(db.Integer)
 
     def to_dict(self):
         return {
             'id': self.id,
             'title': self.title,
-            'description': self.description
+            'description': self.description,
+            'completed': self.completed,
+            'timestamp': self.timestamp
         }
+
 
 
 class User(db.Model):
--- backend/routes/todo_route.py
+++ backend/routes/todo_route.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, request
 from models.task_model import db, Task
 tasks_api = Blueprint('tasks_api', __name__)
 
-
 @tasks_api.route('/tasks', methods=['GET'])
 def get_tasks():
     try:
@@ -17,7 +16,7 @@ def get_task(task_id):
     try:
         task = Task.query.get(task_id)
         if task:
-            return jsonify(task.to_dict()), 200
+            return jsonify(task.to_dict()),200
         else:
             return jsonify({'error': 'Task not found'}), 404
     except Exception as e:
@@ -29,12 +28,12 @@ def add_task():
         data = request.get_json()
         title = data.get('title')
         description = data.get('description')
-
-        task = Task(title=title, description=description)
+        completed = data.get('completed')
+        timestamp = data.get('timestamp')
+        task = Task(title=title, description=description, completed=completed, timestamp=timestamp)
         db.session.add(task)
         db.session.commit()
-
-        return jsonify({'message': 'Task added successfully'}), 200
+        return jsonify({'message': 'Task added successfully', 'id': task.id}), 200
     except Exception as e:
         return jsonify({'error':str(e)}), 404
 
@@ -46,9 +45,13 @@ def update_task(task_id):
             data = request.get_json()
             title = data.get('title')
             description = data.get('description')
+            completed = data.get('completed')
+            timestamp = data.get('timestamp')
 
             task.title = title
             task.description = description
+            task.timestamp = timestamp
+            task.completed = completed
             db.session.commit()
 
             return jsonify({'message': 'Task updated successfully'}), 200
