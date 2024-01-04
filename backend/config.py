3a572a09e38d49b8905e95da032f4077b35774eb
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-04 09:51:51 -0800

    sql path changed

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///backend/instances/todo.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///backend/todo.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
