fa28fc8256001413258071c70a9257c24b152bde
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-03 21:20:53 -0800

    Initial commit for heroku

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
 from models.task_model import db
 from routes.todo_route import tasks_api
 from routes.todo_users import users_api
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = 'my-todo-app/frontend/build')
 CORS(app = app)
 app.config.from_object('config.Config')
 db.init_app(app)
@@ -19,6 +20,10 @@ app.register_blueprint(users_api)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     with app.app_context():
