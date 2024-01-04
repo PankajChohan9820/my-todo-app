758900c9d97e5b32ef361491c694c4f0718b7361
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-03 22:15:49 -0800

    react build added new

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from backend.routes.todo_route import tasks_api
 from backend.routes.todo_users import users_api
 
 
-app = Flask(__name__, static_folder = 'my-todo-app/frontend/build', static_url_path = '')
+app = Flask(__name__, static_folder = 'my-todo-app/frontend/build', static_url_path = '/')
 CORS(app = app)
 app.config.from_object('backend.config.Config')
 db.init_app(app)
@@ -16,10 +16,13 @@ app.register_blueprint(tasks_api)
 app.register_blueprint(users_api)
 
 
-@app.route('/')
-@cross_origin()
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+# @app.route('/')
+# def serve():
+#     return send_from_directory(app.static_folder, 'index.html')
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
 
 if __name__ == '__main__':
     with app.app_context():
