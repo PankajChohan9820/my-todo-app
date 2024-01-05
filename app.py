0f97be08c7640037b596ff8a846b0f3adda7306e
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-05 10:51:01 -0800

    import fix

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from models.task_model import db
 from routes.todo_route import tasks_api
 from routes.todo_users import users_api
 
+
 app = Flask(__name__, static_folder = 'frontend/build', static_url_path = '/')
 CORS(app = app)
 app.config.from_object('config.Config')
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///todo.db'
-    SQLALCHEMY_DATABASE_URI = 'postgres://bfdpjujfjdqyhl:f7dd640e7d911a2fe9f12baa3eea805a3e9df1d43a11509e10ed4ee0965ebe90@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d51o4ppklis9r7'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://bfdpjujfjdqyhl:f7dd640e7d911a2fe9f12baa3eea805a3e9df1d43a11509e10ed4ee0965ebe90@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d51o4ppklis9r7'
