dbbb7388a565da788ed24562a504961fd7175377
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-05 10:11:19 -0800

    postgres url added

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__, static_folder = 'frontend/build', static_url_path = '/')
 CORS(app = app)
-app.config.from_object('config.Config')
+# app.config.from_object('config.Config')
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://bfdpjujfjdqyhl:f7dd640e7d911a2fe9f12baa3eea805a3e9df1d43a11509e10ed4ee0965ebe90@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d51o4ppklis9r7'
 db.init_app(app)
 
 
