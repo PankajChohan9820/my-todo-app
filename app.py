1bb7e8f54ad552517d2073268644d6a114e9919b
PankajChohan9820 <pankajchohan9820@gmail.com>
2024-01-05 10:30:00 -0800

    import fix

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from routes.todo_users import users_api
 
 app = Flask(__name__, static_folder = 'frontend/build', static_url_path = '/')
 CORS(app = app)
-# app.config.from_object('config.Config')
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://bfdpjujfjdqyhl:f7dd640e7d911a2fe9f12baa3eea805a3e9df1d43a11509e10ed4ee0965ebe90@ec2-3-232-218-211.compute-1.amazonaws.com:5432/d51o4ppklis9r7'
+app.config.from_object('config.Config')
 db.init_app(app)
 
 
